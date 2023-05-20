7639afdd359c30e658a5d925165c8b9bd7fb2cf5
Shoirjon <121060727+Shoirjon1@users.noreply.github.com>
2023-05-20 08:55:20 +0300

    Add files via upload
    Signed-off-by: Shoirjon <121060727+Shoirjon1@users.noreply.github.com>

---
+++ zeus/fuck.py
@@ -0,0 +1,7 @@
+from telethon import TelegramClient, events
+import sh.client
+client = sh.client.client
+
+@events.register(events.NewMessage(pattern=".fuck"))
+async def fuck(event):
+	await event.edit("┏━┳┳┳━┳┳┓\n┃━┫┃┃┏┫━┫┏┓\n┃┏┫┃┃┗┫┃┃┃┃\n┗┛┗━┻━┻┻┛┃┃\n┏┳┳━┳┳┳┓┏┫┣┳┓\n┣┓┃┃┃┃┣┫┃┏┻┻┫\n┃┃┃┃┃┃┃┃┣┻┫┃┃\n┗━┻━┻━┻┛┗━━━┛")
---
+++ zeus/mq.py
@@ -0,0 +1,16 @@
+from telethon import TelegramClient, events
+import sh.client
+import os
+client = sh.client.client
+@events.register(events.NewMessage(outgoing=True, pattern='\.svl'))
+async def mq(event):
+	       chat = await event.get_chat()
+	       replied_msg = await event.get_reply_message()
+	       await event.edit("Javob olinmoqda...")
+	       x = await replied_msg.forward_to('@Bilagon_uzbot')
+	       #print(x)
+	       async with client.conversation('@Bilagon_uzbot') as conv:
+	       	xx = await conv.get_response(x.id)
+	       	await client.send_read_acknowledge(conv.chat_id)
+	       	await client.send_message(chat, xx)
+	       	await event.message.delete()
