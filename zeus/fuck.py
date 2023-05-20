808ca6efb8a63fdf31d618068766b8d96d2bfd54
Shoirjon <121060727+Shoirjon1@users.noreply.github.com>
2023-05-20 11:03:41 +0300

    Delete zeus directory
    Signed-off-by: Shoirjon <121060727+Shoirjon1@users.noreply.github.com>

--- zeus/fuck.py
+++
@@ -1,7 +0,0 @@
-from telethon import TelegramClient, events
-import sh.client
-client = sh.client.client
-
-@events.register(events.NewMessage(pattern=".fuck"))
-async def fuck(event):
-	await event.edit("┏━┳┳┳━┳┳┓\n┃━┫┃┃┏┫━┫┏┓\n┃┏┫┃┃┗┫┃┃┃┃\n┗┛┗━┻━┻┻┛┃┃\n┏┳┳━┳┳┳┓┏┫┣┳┓\n┣┓┃┃┃┃┣┫┃┏┻┻┫\n┃┃┃┃┃┃┃┃┣┻┫┃┃\n┗━┻━┻━┻┛┗━━━┛")
--- zeus/test.py
+++
@@ -1,6 +0,0 @@
-from telethon import TelegramClient, events, sync
-
-@events.register(events.NewMessage(pattern=".hello"))
-async def test(event):
-	await event.edit("Assalomu alaykum Telegramingizga ajoib bo'lgan skriptni uladingiz Ushbu skriptning hzmatlaridan foydalanish uchun <.yordam> yoki <.help> deb istalgan chatga yuboring Agar sizda ushbu skriptni kengaytirish va qo'shimcha hizmatlar uchun takliflar bo'lsa skript dasturchisi bilan bog'lanishingiz mumkun Dasturchi❯❯ @Shoirjon_No1 Shu va shunga o'xshash qiziqarli dasturlar va skriptlar Bizning muhokama va yangilillar guruximizdan topiladi ")
-	
